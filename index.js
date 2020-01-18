9529c586af80b8023a8eb4e7c6a9d88a05b3833d
Varun V <varunvirupaksha321@gmail.com>
2020-01-18 17:29:05 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+const square = {
+    area: (a) => (a*a),
+    perimeter : (a) => (4*a)
+}
+
+const calculatesquare = (a) => {
+    console.log(`the value of a is ${a}` + square.area(a) );
+}
+
+calculatesquare(5);

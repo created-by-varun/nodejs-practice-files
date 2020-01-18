71e15e879d75975809dc40c2354b45a60e77d288
Varun V <varunvirupaksha321@gmail.com>
2020-01-18 17:37:38 +0530

    updated index.js

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const calculatesquare = (a) => {
     console.log(`the value of a is ${a} and the area is ` + square.perimeter(a) );
 }
 
+console.log(__filename);
+console.log(__dirname);
+
 calculatesquare(5);
